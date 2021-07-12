7a6effcff2bb6b77ce2b0777434dacf9895b5a82
Mohamed Magdy <mohamedmagdy1101@gmail.com>
2021-07-12 21:46:09 +0200

    Review Screen Changed

--- lib/modules/layouts/reminder_screen.dart
+++ lib/modules/layouts/reminder_screen.dart
@@ -5,14 +5,27 @@ class ReminderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Reminder Screen',
-          style: TextStyle(
-            fontSize: 50,
-            fontWeight: FontWeight.bold,
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.grey,
+            child: Center(
+              child: Card(
+                color: Colors.blue,
+                child: Text(
+                  'Reminder Screen',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
